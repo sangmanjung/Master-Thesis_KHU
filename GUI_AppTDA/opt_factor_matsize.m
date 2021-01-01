5f0daae53c4795e7a26ebf2e058419f56636d925
Sangman Jung <sangmanjung@khu.ac.kr>
2021-01-02 02:13:23 +0900

    Update opt_factor_matsize.m

--- GUI_AppTDA/opt_factor_matsize.m
+++ GUI_AppTDA/opt_factor_matsize.m
@@ -1,10 +1,10 @@
-function [n,p,m]=opt_factor_matsize(dist_matrix)
+function [n,p,m] = opt_factor_matsize(distance_matrix)
 
-m = size(dist_matrix,3);
-if size(dist_matrix,3) == 1
+m = size(distance_matrix,3);
+if size(distance_matrix,3) == 1
     n = 1;
     p = 1;
-elseif size(dist_matrix,3) > 1
+elseif size(distance_matrix,3) > 1
     n = floor(sqrt(m));
     p = ceil(m/n);
 end
